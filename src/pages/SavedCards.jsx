40fcb2ed29ab60cf86aac34708cd7d2738a3fe68
My Name <191200107035@asoit.edu.in>
2024-04-21 04:37:00 +0530

    saved pertoparty show

--- src/pages/SavedCards.jsx
+++ src/pages/SavedCards.jsx
@@ -1,16 +1,41 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import CardList from "../components/cardList/CardList";
-import { Row } from "antd";
-
+import { Menu, Row } from "antd";
+import fillHeart from "../assets/images/filled-heart.svg";
 const SavedCards = () => {
   const cardData = useSelector((state) => state.cardData.cards);
-  console.log(cardData, "sfksalkdfjklsadfj");
+  const items = [
+    {
+      label: "My Activity",
+      key: "activity",
+    },
+  ];
+
   return (
     <>
-      <Row>
+      <Menu selectedKeys={["activity"]} mode="horizontal" items={items} />
+      <div
+        style={{
+          border: "1px solid #f3744b",
+          borderRadius: "20px",
+          padding: "10px 0 ",
+          width: "150px",
+          marginTop: "10px",
+          background: "#fdeeea",
+        }}
+      >
+        <div style={{ display: "flex", justifyContent: "space-between" }}>
+          <img src={fillHeart} alt="fillHeart" />
+          <div style={{ color: "black" }}>{cardData && cardData?.length}</div>
+        </div>
+        <div style={{ color: "#f3744b", textAlign: "center" }}>
+          Saved Property
+        </div>
+      </div>
+      <Row style={{ marginTop: "1rem" }}>
         {cardData &&
-          cardData.map((item, index) => (
+          cardData?.map((item, index) => (
             <React.Fragment key={index}>
               <CardList key={index} itemData={item} />
             </React.Fragment>
